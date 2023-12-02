e77a6afa03c2fcb9adf8e7e13b1a0fd2de074827
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2023-12-02 19:32:16 +0100

    Changed deps

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ import hashlib
 import aiohttp
 import json
 import requests
-from pretty_help import EmojiMenu, PrettyHelp
-
-menu = EmojiMenu(page_left="⬅️", page_right="➡️", remove="❌", active_time=5)
-
-# Custom ending note
-ending_note = "The ending note from {ctx.bot.user.name}\nFor command {help.clean_prefix}{help.invoked_with}"
 
 load_dotenv()
 
@@ -19,8 +13,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 activity = discord.Activity(type=discord.ActivityType.watching, name="for malware")
 
-bot = commands.Bot(command_prefix='!', intents=intents, activity=activity)
-bot.help_command = PrettyHelp(menu=menu, ending_note=ending_note)
+bot = commands.Bot(intents=intents, activity=activity)
 bot.description = f"Hello there! I'm Raspirus. My prefix is {bot.command_prefix}"
 bot.case_insensitive = True
 
@@ -35,17 +28,17 @@ async def hash_helper_method(ctx, result, md5_hash):
         json.dump(result, f, ensure_ascii=False, indent=4)
 
     # Print the response back to the server
-    await ctx.reply(f"Hash {md5_hash} has been marked as malware by "
-                    f"{marked_as_malware_count} out of {len(result['virustotal']['scans'].keys())} antivirus "
-                    f"software\n"
-                    f"Extensive report below:",
-                    file=discord.File('result.json'))
+    await ctx.respond(f"Hash {md5_hash} has been marked as malware by "
+                      f"{marked_as_malware_count} out of {len(result['virustotal']['scans'].keys())} antivirus "
+                      f"software\n"
+                      f"Extensive report below:",
+                      file=discord.File('result.json'))
 
 
-@bot.command(brief="Scans the attached file for malware")
-async def scan(ctx):
-    if len(ctx.message.attachments) == 0:
-        await ctx.reply("No file attached")
+@bot.slash_command(name="scan", description="Scans the attached file for malware")
+async def scan(ctx: discord.ApplicationContext):
+    if ctx.message is None or len(ctx.message.attachments) == 0:
+        await ctx.respond("No file attached")
         return
     attachment_url = ctx.message.attachments[0].url
     file = requests.get(attachment_url)
@@ -59,27 +52,27 @@ async def scan(ctx):
             result = await response.json()
 
     if response.status != 200:
-        await ctx.reply("Issue with the API, please try again later")
+        await ctx.respond("Issue with the API, please try again later")
         return
 
     try:
         await hash_helper_method(ctx, result, md5_hash)
     except KeyError:
-        await ctx.reply("File seems to be safe")
+        await ctx.respond("File seems to be safe")
 
 
-@bot.command(brief="Shows the bots latency")
-async def ping(ctx):
+@bot.slash_command(name="ping", description="Shows the bots latency")
+async def ping(ctx: discord.ApplicationContext):
     # Get the bots latency (ping) to the server
     latency = bot.latency * 1000  # Convert to milliseconds
-    await ctx.reply(f'Ping: {latency:.2f}ms')
+    await ctx.respond(f'Pong: {latency:.2f}ms')
 
 
-@bot.command(brief="Returns the hash of the attached file. Supported algorithms: MD5, SHA256")
-async def hash(ctx, method):
+@bot.slash_command(name="hash", description="Returns the hash of the attached file. Supported algorithms: MD5, SHA256")
+async def hash(ctx: discord.ApplicationContext, method: discord.Option(str, choices=['MD5', 'SHA256'])):
     # Read the file and calculate the hash using the specified algorithm
-    if len(ctx.message.attachments) == 0:
-        await ctx.reply("No file attached")
+    if ctx.message is None or len(ctx.message.attachments) == 0:
+        await ctx.respond("No file attached")
         return
     attachment_url = ctx.message.attachments[0].url
     file = requests.get(attachment_url)
@@ -89,17 +82,17 @@ async def hash(ctx, method):
     elif method.lower() == 'sha256':
         hash_value = hashlib.sha256(file.content).hexdigest()
     else:
-        await ctx.reply('Invalid hash algorithm. Supported algorithms: MD5, SHA256')
+        await ctx.respond('Invalid hash algorithm. Supported algorithms: MD5, SHA256')
         return
 
-    await ctx.reply(f'Hash ({method}): {hash_value}')
+    await ctx.respond(f'Hash ({method}): {hash_value}')
 
 
-@bot.command(brief="Checks if the given MD5 hash is in the database")
-async def check(ctx, md5_hash):
+@bot.slash_command(name="check", description="Checks if the given MD5 hash is in the database")
+async def check(ctx: discord.ApplicationContext, md5_hash: discord.Option(discord.SlashCommandOptionType.string)):
     # Verify if the given hash is in MD5 format
     if len(md5_hash) != 32:
-        await ctx.reply('Invalid MD5 hash format')
+        await ctx.respond('Invalid MD5 hash format')
         return
 
     # Send the hash to the API and wait for the response
@@ -109,14 +102,13 @@ async def check(ctx, md5_hash):
             result = await response.json()
 
     if response.status != 200:
-        await ctx.reply("Issue with the API, please try again later")
+        await ctx.respond("Issue with the API, please try again later")
         return
 
-    marked_as_malware_count = 0
     try:
         await hash_helper_method(ctx, result, md5_hash)
     except KeyError:
-        await ctx.reply("Hash not found in the database")
+        await ctx.respond("Hash not found in the database")
 
 
 @bot.event
